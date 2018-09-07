095f644c96c1e071a591d8532414c1804ad84c9f
He Wang <he@townsquare.agency>
2018-09-07 12:27:52 +1000

    diff two arrays

---
+++ diffTwoArrays.js
@@ -0,0 +1,23 @@
+/**
+ * Intermediate Algorithm Scripting: Diff Two Arrays
+ *
+ * Compare two arrays and return a new array with any items only found in
+ * one of the two given arrays, but not both. In other words, return the
+ * symmetric difference of the two arrays.
+ */
+
+/**
+ *
+ * @param {Array} arr1
+ * @param {Array} arr2
+ */
+function diffArray(arr1, arr2) {
+	const newArr = arr1.concat(arr2);
+	return newArr.filter(item => {
+		// if the last index equals to the first index, then it means there is only one such item in the array.
+		return newArr.indexOf(item) === newArr.lastIndexOf(item);
+	});
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
