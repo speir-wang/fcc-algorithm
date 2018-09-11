e7c0ec8ef1013969d2d6acec2910d8b7aaa1327d
He Wang <he@townsquare.agency>
2018-09-11 13:51:10 +1000

    search and replace is finished

---
+++ searchAndReplace.js
@@ -0,0 +1,31 @@
+/**
+ * Intermediate Algorithm Scripting: Search and Replace
+ *
+ * Perform a search and replace on the sentence using the arguments provided and return the new
+ * sentence.
+ *
+ * First argument is the sentence to perform the search and replace on.
+ * Second argument is the word that you will be replacing (before).
+ * Third argument is what you will be replacing the second argument with (after).
+ *
+ */
+
+function myReplace(str, before, after) {
+	let newAfter = "";
+
+	if (before.slice(0, 1) === before.slice(0, 1).toUpperCase()) {
+		newAfter = after.slice(0, 1).toUpperCase() + after.slice(1);
+	}
+	return str
+		.split(" ")
+		.map(word => {
+			if (word === before) {
+				if (newAfter.length > 0) return newAfter;
+				return after;
+			}
+			return word;
+		})
+		.join(" ");
+}
+
+myReplace("Let us get back to more Coding", "Coding", "algorithms");
